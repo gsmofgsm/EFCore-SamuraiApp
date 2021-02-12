338ae776308b4f5b260807d07c3f9031afded897
Qing Ma <gsmofgsm@hotmail.com>
2021-02-12 23:07:29 +0100

    GetSamurais and AddSamurai

--- SamuraiApp.UI/Program.cs
+++ SamuraiApp.UI/Program.cs
@@ -1,12 +1,41 @@
-﻿using System;
+﻿using SamuraiApp.Data;
+using SamuraiApp.Domain;
+using System;
+using System.Linq;
 
 namespace SamuraiApp.UI
 {
     class Program
     {
-        static void Main(string[] args)
+        private static SamuraiContext _context = new SamuraiContext();
+
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // press F5 to start debugging
+            // do right click to set this project as startup project herebefore
+            _context.Database.EnsureCreated(); // This causes EFcore to read the provider and connection string, check if the db exists, and creates it on the fly
+            GetSamurais("Before Add:");
+            AddSamurai();
+            GetSamurais("After Add:");
+            Console.Write("Press any key...");
+            Console.ReadKey();
+        }
+
+        private static void AddSamurai()
+        {
+            var samurai = new Samurai { Name = "Julie" };
+            _context.Samurais.Add(samurai); // add the samurai object to the context samurai DbSet, so it's an in memory collection of samurais that the context keeps track of
+            _context.SaveChanges(); // will save the data that the context is tracking back to the database
+        }
+        
+        private static void GetSamurais(string text)
+        {
+            var samurais = _context.Samurais.ToList();
+            Console.WriteLine($"{text}: Samurai count is {samurais.Count}");
+            foreach (var samurai in samurais)
+            {
+                Console.WriteLine(samurai.Name);
+            }
         }
     }
 }
