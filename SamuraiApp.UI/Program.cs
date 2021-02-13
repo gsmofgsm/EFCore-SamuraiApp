d9903f37e88741295b2347c3ab6a2a515d79cb06
Qing Ma <gsmofgsm@hotmail.com>
2021-02-13 13:30:02 +0100

    TagWith and Logging

--- SamuraiApp.UI/Program.cs
+++ SamuraiApp.UI/Program.cs
@@ -1,4 +1,5 @@
-﻿using SamuraiApp.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using SamuraiApp.Data;
 using SamuraiApp.Domain;
 using System;
 using System.Linq;
@@ -14,24 +15,28 @@ namespace SamuraiApp.UI
             // press F5 to start debugging
             // do right click to set this project as startup project herebefore
             _context.Database.EnsureCreated(); // This causes EFcore to read the provider and connection string, check if the db exists, and creates it on the fly
-            GetSamurais("Before Add:");
-            AddSamurai();
-            GetSamurais("After Add:");
+            
+            AddSamurais("Julie2", "Sampson2");
+            GetSamurais();
             Console.Write("Press any key...");
             Console.ReadKey();
         }
 
-        private static void AddSamurai()
+        private static void AddSamurais(params string[] names)
         {
-            var samurai = new Samurai { Name = "Julie" };
-            _context.Samurais.Add(samurai); // add the samurai object to the context samurai DbSet, so it's an in memory collection of samurais that the context keeps track of
+            foreach (string name in names)
+            {
+                _context.Samurais.Add(new Samurai { Name = name }); // add the samurai object to the context samurai DbSet, so it's an in memory collection of samurais that the context keeps track of
+            }
             _context.SaveChanges(); // will save the data that the context is tracking back to the database
         }
         
-        private static void GetSamurais(string text)
+        private static void GetSamurais()
         {
-            var samurais = _context.Samurais.ToList();
-            Console.WriteLine($"{text}: Samurai count is {samurais.Count}");
+            var samurais = _context.Samurais
+                .TagWith("ConsoleApp.Program.GetSamurais method")
+                .ToList();
+            Console.WriteLine($"Samurai count is {samurais.Count}");
             foreach (var samurai in samurais)
             {
                 Console.WriteLine(samurai.Name);
